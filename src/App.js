a464f8464ded278b781ad00750e68552bbaa5bcf
Bloom-Sayiorri <bloomsayiorri@gmail.com>
2023-12-24 23:43:42 +0300

    added files and folders

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import { BrowserRouter, Router, Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <></>
+    <BrowserRouter>
+      <Router>
+        <Routes path='/' element={<Home/>}></Routes>
+      </Router>
+    </BrowserRouter>
   );
 }
 
