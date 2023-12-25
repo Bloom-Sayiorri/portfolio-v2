d4831a1b1cfbb4533f6da168113c87c1afc2d7b9
Bloom-Sayiorri <bloomsayiorri@gmail.com>
2023-12-25 20:53:06 +0300

    projects page layout

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
 
-function Projects() {
+function Projects({projects}) {
+  const displayedProject = projects.map((project) => {
+    return (
+      <div>
+        <h3>{project.title}</h3>
+        <h4>{project.stack}</h4>
+        <Link to={project.url}>Live Demo</Link>
+      </div> 
+    )
+  })
   return (
-    <div>Projects</div>
+    <div>
+      {displayedProject}
+    </div>
   )
 }
 
