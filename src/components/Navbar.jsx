abb4483eb5be11f74089198078e9b16c95325f81
Bloom-Sayiorri <bloomsayiorri@gmail.com>
2023-12-28 19:01:50 +0300

    navbar changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import '../styles/navbar.css'
 
 function Navbar() {
   return (
-    <div className='navbar'>
-      <h1>Bloom Sayiorri</h1>
-      <NavLink> </NavLink>
+    <div className='navbar'><Link to='/'className='home_link'>Bloom Sayiorri</Link>
+      <nav className='other_links'>
+        <Link to='/' className="home_link">Projects</Link>
+        <Link to='/' className="home_link">About</Link>
+      </nav>
     </div>
   )
 }
