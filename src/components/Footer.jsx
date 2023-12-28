e6a72d5bc774baaa7e65fd0eb0d352050717bf97
Bloom-Sayiorri <bloomsayiorri@gmail.com>
2023-12-28 19:47:08 +0300

    footer css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,8 +4,15 @@ import '../styles/footer.css'
 function Footer() {
   return (
     <footer className='footer'>
-      <h4>Contact</h4>
-      <h4>Email</h4>
+      <section className='first_section'>
+        <h4>Contact</h4>
+        <h4>Email</h4>
+      </section>
+      <section className='second_section'>
+        <h4>Home</h4>
+        <h4>Skills</h4>
+        <h4>About</h4>
+      </section>
     </footer>
   )
 }
