2bcd4e14ec56d060fc8df78dda46d08f0f79166a
Bloom-Sayiorri <bloomsayiorri@gmail.com>
2023-12-30 23:16:24 +0300

    skills.jsx layout

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
 
-function Skills() {
+function Skills({skills}) {
+  const card = () => {
+    skills.map((skill) => {
+      return (
+        <section>
+        {/* <h4>{title}</h4>
+        <p>{language}</p>
+        <img src={''} alt=''/> */}
+      </section>
+      )
+    })
+  }
+
   return (
     <div className='skills'>
       <h3>Skills</h3>
-      <section>
-        <h4>{title}</h4>
-        <p>{language}</p>
-        <img src={''} alt=''/>
-      </section>
+      {skills}
     </div>
   )
 }
